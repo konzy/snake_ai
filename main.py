ce1bf042e320d92894c40145bf47a3637179c04c
Brian <bkonzman@gmail.com>
2018-11-08 00:40:37 -0500

    added some keras code, and some open ai gym render stuff that doesn't work, kinda just stubbed some stuff out

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 
 
 # board coordinates- (0, 0) at the top left
+import copy
+import pickle
+import random as rand
+import keras
+
 from snake_game import SnakeGame
 
 
 def main():
 
-    snake_game = SnakeGame(6, 6)
-    cont = True
+    # Initialize snake stuff
+    seed = rand.random
+    y = 6
+    x = 10
+    num_agents = 500
+    generation = 1
 
-    while cont:
-        snake_game.print_board()
-        cont = snake_game.move(0)
-        print(snake_game.score + snake_game.distance_score)
+    brains = [num_agents]
+
+    snake_game = SnakeGame(y, x, seed)
+
+    for brain in brains:
+        brain = create_brain()
+
+    for i in range(num_agents):
+        print('Start of generation ' + str(generation) + ' agent ' + str(i))
+        turn = 0
+        game = copy.deepcopy(snake_game)
+
+        # Writes entire game object to file, could be more efficient but, whatever.
+        pickle.dump(game, open("gen" + str(generation) + '/agent' + str(i) + 'turn' + str(turn)))
+
+
+def create_brain():
+    keras.layers.Conv2D(20, (3, 3), strides=(1, 1), padding='valid', data_format=None,
+                        dilation_rate=(1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform',
+                        bias_initializer='zeros')
 
 
 main()
--- snake_game.py
+++ snake_game.py
@@ -21,10 +21,12 @@ class SnakeGame:
         ';': (1, 0),  # down
     }
 
-    def __init__(self, play_width, play_height):
+    def __init__(self, play_width, play_height, seed):
 
         self.play_width = play_width
         self.play_height = play_height
+
+        rand.seed = seed
 
         self.wall_width = play_width + 2
         self.wall_height = play_height + 2
@@ -51,12 +53,19 @@ class SnakeGame:
         self._place_apple()
 
         self.distance_score = 0
-
         self.score = 0
+        self.viewer = None
 
     def print_board(self):
         # print board
         print(self.walls + self.snake - self.apple)
+
+    def get_board(self):
+        walls = self.walls.reshape((self.wall_height, self.wall_width, 1))
+        snake = self.snake.reshape((self.wall_height, self.wall_width, 1))
+        apple = self.apple.reshape((self.wall_height, self.wall_width, 1))
+
+        return np.concatenate((walls, snake, apple))
 
     # returns 0 to continue, 1 for end of game
     def move(self, m):
@@ -133,6 +142,65 @@ class SnakeGame:
         tmp_direction = self.char_to_direction.get(tmp, '')
         if tmp_direction != '':
             return tmp_direction
+
+    #  ripped off from https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+    def render(self, mode='human'):
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.x_threshold * 2
+        scale = screen_width / world_width
+        carty = 100  # TOP OF CART
+        polewidth = 10.0
+        polelen = scale * (2 * self.length)
+        cartwidth = 50.0
+        cartheight = 30.0
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+            axleoffset = cartheight / 4.0
+            cart = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            self.carttrans = rendering.Transform()
+            cart.add_attr(self.carttrans)
+            self.viewer.add_geom(cart)
+            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
+            pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            pole.set_color(.8, .6, .4)
+            self.poletrans = rendering.Transform(translation=(0, axleoffset))
+            pole.add_attr(self.poletrans)
+            pole.add_attr(self.carttrans)
+            self.viewer.add_geom(pole)
+            self.axle = rendering.make_circle(polewidth / 2)
+            self.axle.add_attr(self.poletrans)
+            self.axle.add_attr(self.carttrans)
+            self.axle.set_color(.5, .5, .8)
+            self.viewer.add_geom(self.axle)
+            self.track = rendering.Line((0, carty), (screen_width, carty))
+            self.track.set_color(0, 0, 0)
+            self.viewer.add_geom(self.track)
+
+            self._pole_geom = pole
+
+        if self.state is None: return None
+
+        # Edit the pole polygon vertex
+        pole = self._pole_geom
+        l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
+        pole.v = [(l, b), (l, t), (r, t), (r, b)]
+
+        x = self.state
+        cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
+        self.carttrans.set_translation(cartx, carty)
+        self.poletrans.set_rotation(-x[2])
+
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
 
 
 def distance_formula(coord_1, coord_2):
